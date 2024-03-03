7dfd0ccd3348a46c3acba1911bf21a6556f14bbe
Ghulam qadir <154546196+ghulamqadir10@users.noreply.github.com>
2024-03-03 17:22:42 +0500

    Add files via upload

---
+++ chap2.js
@@ -0,0 +1,31 @@
+// // // assignment 02
+let username;
+let myname =" ghulam qadir"
+let message="hello"
+message= "Hello World"
+alert(message)
+let student_bio_data;
+name ="Ghulam Qadir"
+age = 20;
+Qualification="web and mobile app development"
+alert(name)
+alert(age)
+alert(Qualification)
+let who_know_my_pizza;
+who_know_my_pizza="PIZZA" + "\n"+ "PIZZ" +"\n"+ "PIZ" +"\n"+ "PI" +"\n"+ "P"
+alert(who_know_my_pizza)
+// et Email;
+let Email="my Email address is"
+let Email_address;
+Email_address=" example@example.com"
+// let result=Email.concat("",Email_address)
+let result=Email.concat(Email_address);
+alert(result)
+let exam;
+exam= "i am trying to learn from the book"
+let book=" A smarter way to learn JavaScript"
+let example=exam.concat(book);
+alert(example)
+document.write("<h2 class='border2'>yeah i can write HTML content through java script"+ "<br>" + "<br>" + "<br>")
+let an_image = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(an_image)
